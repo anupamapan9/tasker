189ddcd4ba899dc5f34b830d5758f3b9dfd24181
anupamapan9 <anupamapan0905@gmail.com>
2024-01-14 13:21:53 +0600

    added hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import HeroSection from "./Components/HeroSection";
 
 function App() {
   return (
     <>
       <Header />
+      <HeroSection />
+      <Footer />
     </>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import LogoLWS from "../assets/lws-logo-en.svg";
 export default function Header() {
   return (
