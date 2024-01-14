3a1b7eaf9d426fc8a9ea6821ee50df3fd4aa8455
anupamapan9 <anupamapan0905@gmail.com>
2024-01-14 13:27:35 +0600

    added hero sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import "./App.css";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import HeroSection from "./Components/HeroSection";
+import TaskBoard from "./Components/Tasks/TaskBoard";
 
 function App() {
   return (
     <>
       <Header />
-      <HeroSection />
+      <div className="flex flex-col items-center justify-center">
+        <HeroSection />
+        <TaskBoard />
+      </div>
+
       <Footer />
     </>
   );
